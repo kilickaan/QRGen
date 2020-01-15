a3543eaa08db22dac9c1a7e224781bf69e8932ea
Kaan <slothico@gmail.com>
2020-01-15 17:03:37 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.picBarcode = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBarcodeValue = new System.Windows.Forms.RichTextBox();
+            this.stringBox = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBarcode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerate.Location = new System.Drawing.Point(12, 446);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(114, 44);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // picBarcode
+            // 
+            this.picBarcode.BackColor = System.Drawing.Color.White;
+            this.picBarcode.Location = new System.Drawing.Point(12, 45);
+            this.picBarcode.Name = "picBarcode";
+            this.picBarcode.Size = new System.Drawing.Size(427, 248);
+            this.picBarcode.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picBarcode.TabIndex = 3;
+            this.picBarcode.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(325, 446);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(114, 44);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 22);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "QRCode";
+            // 
+            // txtBarcodeValue
+            // 
+            this.txtBarcodeValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBarcodeValue.Location = new System.Drawing.Point(12, 299);
+            this.txtBarcodeValue.Name = "txtBarcodeValue";
+            this.txtBarcodeValue.Size = new System.Drawing.Size(427, 130);
+            this.txtBarcodeValue.TabIndex = 6;
+            this.txtBarcodeValue.Text = "";
+            // 
+            // stringBox
+            // 
+            this.stringBox.Location = new System.Drawing.Point(488, 45);
+            this.stringBox.Name = "stringBox";
+            this.stringBox.Size = new System.Drawing.Size(504, 384);
+            this.stringBox.TabIndex = 7;
+            this.stringBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 509);
+            this.Controls.Add(this.stringBox);
+            this.Controls.Add(this.txtBarcodeValue);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.picBarcode);
+            this.Controls.Add(this.btnGenerate);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picBarcode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.PictureBox picBarcode;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox txtBarcodeValue;
+        private System.Windows.Forms.RichTextBox stringBox;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [Obsolete]
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            GenerateBarcode();
+
+        }
+
+        [Obsolete]
+        public void GenerateBarcode()
+        {
+            try
+            {
+                String data = txtBarcodeValue.Text;
+
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qrCodeData = qrGenerator.CreateQrCode(data, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qrCodeData);
+                Bitmap qrCodeImage = qrCode.GetGraphic(10);
+
+
+
+                Base64QRCode qrCode2 = new Base64QRCode(qrCodeData);
+                string qrCodeImageAsBase64 = qrCode2.GetGraphic(10);
+
+
+
+
+                stringBox.Text = qrCodeImageAsBase64.ToString().Trim();
+                picBarcode.Image = qrCodeImage;
+
+                String Imagename = "C://BarcodeData//" + data + "_" + System.DateTime.Now.ToString("ddMMyyyyHHMMss") + ".png";
+                if (!File.Exists(Imagename)) File.Create(Imagename).Close();
+                picBarcode.Image = System.Drawing.Image.FromFile(@"C://BarcodeData//" + data + "_" + System.DateTime.Now.ToString("ddMMyyyyHHMMss") + ".png");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fdb = new FolderBrowserDialog();
+            fdb.Description = "fdf";
+            if (fdb.ShowDialog() == DialogResult.OK)
+            {
+                picBarcode.Image.Save(@fdb.SelectedPath + "\\Image" + System.DateTime.Now.ToString("ddMMyyyyHHmmss") + ".jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                MessageBox.Show("Barcode Image saved Successfully. Path : " + fdb.SelectedPath);
+            }
+        }
+
+        
+    }
+}
